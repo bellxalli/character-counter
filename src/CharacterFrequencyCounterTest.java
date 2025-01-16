10003fa82bf14e73727557b2066f0072228828f4
X BELL <194058603+bellxalli@users.noreply.github.com>
2025-01-16 11:15:15 -0800

    Fixed One Bug

--- src/CharacterFrequencyCounterTest.java
+++ src/CharacterFrequencyCounterTest.java
@@ -3,5 +3,27 @@ import static org.junit.Assert.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class CharacterFrequencyCounterTest {
-    
+    @Test
+    public void testLAppearsThreeTimesInHelloWorld()
+    {
+        //arrange: set up test
+        CharacterFrequencyCounter counter = new CharacterFrequencyCounter("hello world");
+        
+        //act: making something happen calling a method usually
+        int actualCount = counter.getFrequency('l');
+
+        //assert: get results and check that they match expected results
+        assertEquals(3, actualCount);
+
+    }
+
+    @Test
+    public void testNoneOccurringCharacterPercentageIsZero()
+    {
+        CharacterFrequencyCounter counter = new CharacterFrequencyCounter("hi there");
+
+        double actualPercentage = counter.getRelativePercentage('z');
+
+        assertEquals(0.0, actualPercentage, 0.001);
+    }
 }
